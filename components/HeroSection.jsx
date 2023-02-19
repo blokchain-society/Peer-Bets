497bf006a79d2515f971d95aa577b2137f06924c
Neeraj Choubisa <neerajchoubisa876@gmail.com>
2023-02-19 12:13:31 +0530

    Update ui

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -51,7 +51,7 @@ export default function HeroSection() {
             className="font-bold font-fontDM font-xl  text-center text-white bg-gradient-to-r from-btn-blue via-btn-purple to-btn-blue   
         animate-glow bg-300 mt-4 p-3 rounded-full"
           >
-            <Link href="createroom"><h1 className="whitespace-nowrap d-flex justify-between" ><span className="mt-2 mx-2">Join-Room </span><GrAddCircle size="40"  /></h1></Link>
+            <Link href="joinroom"><h1 className="whitespace-nowrap d-flex justify-between" ><span className="mt-2 mx-2">Join-Room </span><GrAddCircle size="40"  /></h1></Link>
           </button>
         </div>
       </div>
